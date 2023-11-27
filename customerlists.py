a481e62e9c994c8d44c8689eba545e52d60ee674
abigailbang-pvcc <145607595+abigailbang-pvcc@users.noreply.github.com>
2023-11-27 15:16:39 -0500

    Add files via upload

---
+++ customerlists.py
@@ -0,0 +1,34 @@
+# Name: Abigail Bang
+# Prog Purpose: Read in a list of customers and display a report
+# increate the amount owed by %10
+def main():
+    read_in_cust_file()
+    display_cust_list()
+
+def read_in_cust_file():
+    global cust
+    cust_data = open("customer_data_file.csv", "r")
+    cust_in = cust_data.readlines()#read the entire file into cust_in
+    cust_data.close()
+
+#split the data into fields
+    cust = []
+    for i in cust_in: #separate the data by commas and add to the list
+        cust.append(i.split(","))
+
+def display_cust_list():
+    total = 0
+    fcurrency = "8,.2f"
+    line = "----------------------------------------"
+
+    print(line)
+    print("***** CUSTOMER SALES REPORT ********\n")
+    for i in range(len(cust)):
+        amt_owed = float(cust[i][2]) * 1.10 #increase amount by %10
+        total+= amt_owed
+        print(cust[i][1] + "    \t" + cust[i][0]+ "\t"+ format(amt_owed,fcurrency))
+
+    print(line)
+    print("****** TOTAL AMOUNT: \t" + format(total, fcurrency))
+
+main()
